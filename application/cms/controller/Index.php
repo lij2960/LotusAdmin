b26808c9055454197883e6c0da5193a482b86b7f
wenhainan <whndeweilai@163.com>
2017-10-17 09:32:40 +0800

    修改链接

---
+++ application/cms/controller/Index.php
@@ -0,0 +1,28 @@
+<?php
+namespace app\cms\controller;
+
+use think\Controller;
+use think\Db;
+class Index extends Controller
+{
+    function test()
+    {
+        $arr = Db::name('auth_rule')
+        	->select();
+        dump($this->array2level($arr));
+    }
+    function array2level($array,$pid=0,$level=1){
+    	static $list = [];
+    	foreach($array as $v){
+    		if($v['pid']==$pid){
+    			$v['level'] = $level;
+    			$list[]     = $v;
+    			dump($list);
+    			$this->array2level($array, $v['id'], $level + 1); 
+    		}
+    	}
+    	return $list;
+    }
+
+
+}
