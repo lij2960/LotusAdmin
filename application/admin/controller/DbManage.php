278758f19e5354dc1e3b76ff61178a2b0b63424d
wenhainan <whndeweilai@163.com>
2017-10-19 16:48:51 +0800

    db

--- application/admin/controller/DbManage.php
+++ application/admin/controller/DbManage.php
@@ -6,9 +6,10 @@ class DbManage extends Main
 {
   function index(){
     $db_names =  Db::getTables();
+    $count = count($db_names);
     $this->assign('db_names',$db_names);
+    $this->assign('count',$count);
     return $this->fetch();
   }
-
-
+  
 }
